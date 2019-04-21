31016315eb91e82e9636988a86dd1250883981db
Vadis00 <37323791+Vadis00@users.noreply.github.com>
2019-04-21 17:01:30 +0300

    Add files via upload

---
+++ lab 1/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App1
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+            picker.ItemsSource = new List<string>(itSpesiality);
+        }
+
+        int item;
+        string[] itSpesiality = new string[] { "Тестировщик", "Мобильный разработчик", "Верстальщик", "Front-end", "Back-end" };
+        string[] requrment = new string[] { "Занимается тестировкой проектов", "Разрабатывает программы для мобильных устройств",
+             "Занимается ветсткой проектов", "Занимается фронтенд разработкой", "Занимается бекенд разработкой" };
+
+        private void BtnChange_Clicked(object sender, EventArgs e)
+        {
+            lblChange.Text = requrment[item];
+        }
+
+        private void Picker_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            item = picker.SelectedIndex;
+        }
+    }
+}
